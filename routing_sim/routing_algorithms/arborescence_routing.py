d48126a567ac4e0d45902f1fb6a250baac2c5704
Leon A. Okida Gonçalves <laog19@inf.ufpr.br>
2025-11-03 21:06:41 -0300

    add: first version of tarjan algorithm; has failures

--- routing_sim/routing_algorithms/arborescence_routing.py
+++ routing_sim/routing_algorithms/arborescence_routing.py
@@ -1,68 +1,108 @@
 # The arborecence-based routing algorithm, using a precomputed arborescence packing
 # Author: Leon Okida
-# Last modification: 10/20/2025
+# Last modification: 11/03/2025
 
 import networkx as nx
 from routing_sim.routing_algorithms.interface import RoutingAlgorithm
+import copy
 
 class ArborescenceRouting(RoutingAlgorithm):
     def __init__(self):
         super().__init__("ArborescenceRouting")
-        self.arborescence_packing = None
+        self.arborescence_packing = dict()
+        self.arborescence_index = 0
+
+    def _condition_1(self, r: str | int, c: int, topology: nx.DiGraph) -> bool:
+        # Tests condition 1 of Tarjan's Algorithm
+        aux = copy.deepcopy(topology)
+        source = "condition_1_s"
+        aux.add_node(source)
+
+        for i in range(c):
+            intermediate = f"condition_1_i_{i}"
+            aux.add_edge(source, intermediate, capacity=1)
+            aux.add_edge(intermediate, r, capacity=1)
+
+        for node in sorted(topology.nodes()):
+            if nx.algorithms.maximum_flow_value(aux, source, node, flow_func=nx.algorithms.flow.edmonds_karp) < c:
+                return False
+        
+        return True
+    
+    def _condition_4(self, r: str | int, tail: str | int, head: str | int, c: int, j: int, topology: nx.DiGraph, used_edges: set) -> bool:
+        # Tests condition 4 of Tarjan's Algorithm
+        aux = copy.deepcopy(topology)
+        for edge in used_edges:
+            aux.remove_edge(*edge)
+
+        for i in range(j, c):
+            intermediate = f"condition_4_i_{i}"
+            aux.add_edge(tail, intermediate, capacity=1)
+            aux.add_edge(intermediate, r, capacity=1)
+
+        return nx.algorithms.maximum_flow_value(aux, tail, head, flow_func=nx.algorithms.flow.edmonds_karp) >= c - j
+
+    def _compute_rooted_arborescences(self, r: str | int, c: int, topology: nx.Graph) -> list[nx.DiGraph]:
+        # Uses the Tarjan algorithm to generate c r-rooted arborescences
+        
+        # Transforms topology into a digraph
+        topology_digraph = topology.to_directed()
+
+        used_edges = set()
+        arborescences = list()
+
+        # Checks if it's possible to compute c r-rooted arborescences
+        if not self._condition_1(r, c, topology_digraph):
+            raise Exception(f"Failed condition 1 to create {c} {r}-rooted arborescences")
+        
+        for j in range(1, c + 1):
+            arbo = nx.DiGraph()
+            arbo.add_node(r)
+
+            usable_edges = set(topology_digraph.edges()) - used_edges
+
+            while set(arbo.nodes()) != set(topology_digraph.nodes()):
+                progress = False
+
+                # Iterates over usable edges
+                for u, v in sorted(usable_edges):
+                    # Finds candidate e*
+                    if u in arbo and v not in arbo:
+                        # Removes from usable edges
+                        usable_edges.remove((u, v))
+
+                        # Checks if it's possible to add the edge to the arborescence
+                        if self._condition_4(r, u, v, c, j, topology_digraph, used_edges):
+                            arbo.add_edge(u, v)
+                            progress = True
+                            break
+
+                if not progress:
+                    raise Exception(f"The algorithm stagnated while building the #{j} {r}-rooted arborescence")
+
+            # Updates the used edges set
+            used_edges.update(arbo.edges())
+            arborescences.append(arbo)
+            print(f"Created #{j} {r}-rooted arborescence")
+        
+        return arborescences
 
     def compute_arborescence_packing(self, topology: nx.Graph) -> None:
         # Computes the arborescence packing
         connectivity_c = nx.edge_connectivity(topology)
-        arborescence_packing = {}
 
         # Iterate over every possible destination d
         for d in topology.nodes:
-            d_arborescences = {}
-            # Iterate over every possible source s (which is every node except d)
-            for s in topology.nodes:
-                if s == d:
-                    continue
-
-                # Find the required number of edge-disjoint paths (SEDPs)
-                try:
-                    # Use NetworkX's flow capabilities to find edge-disjoint paths
-                    paths_iterator = nx.edge_disjoint_paths(topology, s, d)
-                    
-                    disjoint_paths = list(paths_iterator)[:connectivity_c]
-                    
-                    # Extract the next hop for each path
-                    next_hops = []
-                    for path in disjoint_paths:
-                        if len(path) > 1:
-                            next_hops.append(path[1]) # The second node in the path is the next hop
-                    
-                    d_arborescences[s] = next_hops
-                    
-                except nx.NetworkXNoPath:
-                    # No path exists (shouldn't happen in a connected graph)
-                    d_arborescences[s] = []
-                
-            arborescence_packing[d] = d_arborescences
-            
-        self.arborescence_packing = arborescence_packing # Store the computed structure
+            d_arborescences = self._compute_rooted_arborescences(d, connectivity_c, topology)
+            self.arborescence_packing[d] = d_arborescences
 
     def calculate_next_hop(self, source: str | int, dest: str | int, global_topology: nx.Graph, visited_names: set) -> tuple:      
         # Calculates the next hop based on the arborescences
         if dest not in self.arborescence_packing:
             return None, float('-inf')
 
-        potential_next_hops = self.arborescence_packing.get(dest, {}).get(source, [])
-        
-        if not potential_next_hops:
-            return None, float('-inf')
-        # Iterates through arborescences to find a next hop
-        for arbo_index in range(len(potential_next_hops)):
-            next_hop = potential_next_hops[arbo_index]
-            if next_hop not in visited_names:
-                try:
-                    edge_cost = global_topology[source][next_hop].get('weight', 1)
-                    return next_hop, -edge_cost 
-                except KeyError:
-                    continue
+        # Returns the predecessor of source in the arborescence corresponding to dest (it's the next hop in the path to dest)
+        for next_hop in self.arborescence_packing[dest][self.arborescence_index].predecessors(source):
+            return next_hop, 1
 
         return None, float('-inf')
