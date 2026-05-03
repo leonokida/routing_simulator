778df0a4a7b383a4e9f403af2310ba44077e1456
Leon A. Okida Gonçalves <laog19@inf.ufpr.br>
2026-05-03 16:20:05 -0300

    finishes refactor, recreates experiment script

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo "Running experiment on the Géant Topology"
-python3 internet_topologies_experiment.py topologies/geant.txt results/geant.csv >> geant_results.out
-
-echo "Running experiment on the RNP Topology"
-python3 internet_topologies_experiment.py topologies/rnp_equal.txt results/rnp.csv >> rnp_results.out
-
-echo "Running experiment on the Internet2 Topology"
-python3 internet_topologies_experiment.py topologies/internet2.txt results/internet2.csv >> internet2_results.out
+echo "Running experiment"
+python3 experiment.py >> experiment.out
